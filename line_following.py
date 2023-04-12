be75bd28956582a6211e0c970c9cdf70e3fcad54
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2023-04-12 17:06:23 -0600

    fix speed 10

--- line_following.py
+++ line_following.py
@@ -98,7 +98,7 @@ try:
         x=0
         y=0
         print("loop")
-        rows, cols = np.where(edge == i) # extract row and column numbers for each pixel
+        rows, cols = np.where(edge == 255) # extract row and column numbers for each pixel
         rows, cols = np.transform.xy(edge.transform, rows, cols)
         print(rows)
         for i in range(100,480-100):
