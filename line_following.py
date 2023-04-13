c86ad68c36ef818488d36f2fddd0eea20da8bc8a
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2023-04-12 18:10:13 -0600

    fix speed 10

--- line_following.py
+++ line_following.py
@@ -128,7 +128,7 @@ try:
         xavg = y/total
         cv2.rectangle(edge,(180,100),(640-10,480-100),(155,155,155),5)
         cv2.circle(edge, (int(xavg), int(yavg)), 15, (155,0,0), 10)
-        cofy = int(100)
+        cofy = int(300)
         cofx = int((420-60)/2)
         cof = (cofx, cofy)
         cv2.circle(edge, cof, 10, (255,0,0), 5)
