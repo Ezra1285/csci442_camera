1041f21e9b5d78e258e3640ba83814f2824a45cf
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2023-04-26 11:41:33 -0600

    add code

--- line_crossing.py
+++ line_crossing.py
@@ -14,13 +14,10 @@
 #   move towards blue
 #   print when in mining area
 
-
 import pyrealsense2 as rs
-import maestro
-import tkinter as tk
 import numpy as np
+import maestro
 import cv2
-import time
 
 # Configure depth and color streams
 pipeline = rs.pipeline()
@@ -42,85 +39,155 @@ if not found_rgb:
     exit(0)
 
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
 if device_product_line == 'L500':
     config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
 else:
     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
 # Start streaming
-profile = pipeline.start(config)
-time.sleep(3)
-frames = pipeline.wait_for_frames()
-depth_frame = frames.get_depth_frame()
-color_frame = frames.get_color_frame()
-color = np.asanyarray(color_frame.get_data())
-# bbox = (287, 23, 86, 320)
+pipeline.start(config)
 
-# Getting the depth sensor's depth scale (see rs-align example for explanation)
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
-# print("Depth Scale is: " , depth_scale)
+def crop_center(img,cropx,cropy):
+    y,x = img.shape
+    startx = x//2-(cropx//2)
+    starty = y//2-(cropy//2)    
+    return img[starty:starty+cropy,startx:startx+cropx]
 
+def move_forward():
+    robot_controll.setTarget(2, 6000)
+    robot_controll.setTarget(0, 5250)
+    print("forward")
 
-# We will be removing the background of objects more than
-#  clipping_distance_in_meters meters away
-# clipping_distance_in_meters = 1 #1 meter
-# clipping_distance = clipping_distance_in_meters / depth_scale
+def stop():
+    robot_controll.setTarget(2, 6000)
+    robot_controll.setTarget(0, 6000)
+    print("stop")
 
-# Create an align object
-# rs.align allows us to perform alignment of depth frames to others frames
-# The "align_to" is the stream type to which we plan to align depth frames.
-align_to = rs.stream.color
-align = rs.align(align_to)
+def right_forward():
+    robot_controll.setTarget(0, 5200)
+    robot_controll.setTarget(2, 7000)
+    print("left foward")
 
-# Uncomment the line below to select a different bounding box
-# bbox = cv2.selectROI(color, False)
+def left_forward():
+    robot_controll.setTarget(0, 5200)
+    robot_controll.setTarget(2, 5000)
+    print("right forward")
 
-# Initialize tracker with first frame and bounding box
+def right():
+    robot_controll.setTarget(0, 6000)
+    robot_controll.setTarget(2, 7000)
+    print("left")
+
+def left():
+    robot_controll.setTarget(0, 6000)
+    robot_controll.setTarget(2, 5000)
+    print("right")
+
 robot_controll = maestro.Controller()
 robot_controll.setAccel(0,60)
 robot_controll.setSpeed(0, 10)
 robot_controll.setTarget(0, 6000)
 
-
-counter = 0
-motor_value = 6000
 try:
     while True:
-        
         # Wait for a coherent pair of frames: depth and color
         frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
         color_frame = frames.get_color_frame()
-        if not depth_frame or not color_frame:
+        if not color_frame:
             continue
-        
-        # Convert images to numpy arrays
-        depth_image = np.asanyarray(depth_frame.get_data())
-        color_image = np.asanyarray(color_frame.get_data())
+        # Convert color_frameimages to numpy arrays
         color = np.asanyarray(color_frame.get_data())
-        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
+        print("color")
+        #fix lighting      	
+        gray = cv2.cvtColor(color, cv2.COLOR_BGR2GRAY)
+        gray = cv2.cvtColor(gray, cv2.COLOR_GRAY2BGR)
+        blur = cv2.blur(gray, (30,30))
+        diff = cv2.absdiff(color, blur)
+        diff = cv2.blur(color, (5,5))
+        edge = diff
+        print("blur")
+        # thresh = cv2.inRange(diff, 100, 150)
+        #start line following
+        t_lower = 100  # Lower Threshold
+        t_upper = 150  # Upper threshold
 
-        depth_colormap_dim = depth_colormap.shape
-        color_colormap_dim = color_image.shape
+        # Applying the Canny Edge filter
+        print("edge")
+        # edge = cv2.Canny(diff, t_lower, t_upper)
+        #find COG
+        total = 0
+        x=0
+        y=0
+        print("loop")
+        edge = edge[40:440,150:450]
+        rows, cols = np.where(edge == 255) # extract row and column numbers for each pixel
 
-        # If depth and color resolutions are different, resize color image to match depth image for display
-        if depth_colormap_dim != color_colormap_dim:
-            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
-            images = np.hstack((resized_color_image, depth_colormap))
-        else:
-            images = np.hstack((color_image, depth_colormap)) 
-        cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
-        cv2.imshow('RealSense', images)
-        counter += 1
-        key = cv2.waitKey(1)
+        # rows, cols = transform.xy(edge.transform, rows, cols)
+        # np.set_printoptions(threshold=12000)
+        # for i in range(100,480-100):
+        #     for j in range(180, 640-180):
+        #         print(edge[i,j])
+        #         if(edge[i,j] > 100):
+        #             x += i
+        #             y += j
+        #             total +=1
+        #         pass
+        x = rows.sum()
+        y = cols.sum()
+        print(x)
+        print(y)
+        total = rows.size
+        print(total)
+        if total == 0:
+            total = 1
+        yavg = x/total
+        xavg = y/total
+        cv2.rectangle(edge,(180,100),(640-10,480-100),(155,155,155),5)
+        cv2.circle(edge, (int(xavg), int(yavg)), 15, (155,0,0), 10)
+        cofy = int(300)
+        cofx = int((440-40)/2)
+        cof = (cofx, cofy)
+        cv2.circle(edge, cof, 10, (255,0,0), 5)
+        cog = (xavg,yavg)
+
         
+        #TODO
+        #move towards COG
+        # xdif = cof[0] - cog[0]
+        # ydif = cof[1] - cog[1]
+        # if ydif <0:
+        #     stop()
+        # elif xdif <-35:
+        #     if ydif >10:
+        #         left_forward()
+        #     else:
+        #         right()
+        # elif xdif >35:
+        #     if ydif > 10:
+        #         right_forward()
+        #     else:
+        #         left()
+
+        # elif ydif > 0:
+        #     move_forward()
+        # else:
+        #     stop()
+        # if(total <3):
+        #     stop()
+        cv2.imshow('RealSense', edge)
+        key = cv2.waitKey(1)
         if(key == 27):
             break
         
 
+
+
+
 finally:
-    # Stop streaming
+    robot_controll.setAccel(0,60)
+    robot_controll.setSpeed(0, 10)
     robot_controll.setTarget(0, 6000)
-    robot_controll.close()
+    robot_controll.setTarget(2, 6000)
+    # Stop streaming
     pipeline.stop()
