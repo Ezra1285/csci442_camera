bdba3e5a73ffe2b28391ace527f1766b13bd5e4d
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2023-04-26 12:18:29 -0600

    add code

--- line_crossing.py
+++ line_crossing.py
@@ -115,12 +115,14 @@ try:
         gray = cv2.cvtColor(edge, cv2.COLOR_BGR2GRAY)
         
         # Find Canny edges
+        print("edge")
         edged = cv2.Canny(gray, 30, 200)
         cv2.waitKey(0)
         
         # Finding Contours
         # Use a copy of the image e.g. edged.copy()
         # since findContours alters the image
+        print("contuors")
         contours, hierarchy = cv2.findContours(edged, 
             cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
