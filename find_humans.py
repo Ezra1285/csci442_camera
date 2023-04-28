c296f5f28b8544ed200584632e893a6ffe614b1c
Ezra1285 <ezraskoog@yahoo.com>
2023-04-28 16:14:40 -0600

    find_humans1

--- find_humans.py
+++ find_humans.py
@@ -3,10 +3,11 @@ import cv2 as cv
 
 webcam = cv.VideoCapture(0)
 
-while(True):
-    _, imageFrame = webcam.read()
-
-    cv.imshow("Window", imageFrame)
+while(1):
+    _, frame = webcam.read()
+    frame = cv.resize(frame, None, fx=.75,fy=.75, interpolation=cv.INTER_AREA)
+    cv.imshow("Window", frame)
     key = cv.waitKey(1)
     if(key == 27):
+        cv.destroyAllWindows()
         break
