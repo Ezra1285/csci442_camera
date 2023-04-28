a5cca360d4a2107d6243f9132497b9ca2f82b8ab
Ezra1285 <ezraskoog@yahoo.com>
2023-04-28 17:53:26 -0600

    Base robot movement class14

--- test_movement.py
+++ test_movement.py
@@ -1,8 +1,8 @@
 import control_robot
 
 robot = control_robot.robot()
-for i in range(4):
-    robot.right()
+for i in range(10):
+    robot.left()
     # print("Loop " , i)
     # robot.waistRight()
 
