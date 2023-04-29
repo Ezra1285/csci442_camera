de954783cec4a73c767b2210b869de138a512fc6
Ezra1285 <ezraskoog@yahoo.com>
2023-04-28 18:24:43 -0600

    fix7

--- test_movement.py
+++ test_movement.py
@@ -8,12 +8,13 @@ bot.setAccel(0,60)
 bot.setSpeed(0, 10)
 bot.setTarget(0, 6000)
 
-bot.setTarget(2, 7000)
-bot.setTarget(0, 5500)
+# bot.setTarget(2, 7000)
+bot.setTarget(0, 5000)
 
 
 bot.setTarget(0, 6000)
 bot.setTarget(2, 6000)
+bot.close()
 # robot = control_robot.robot()
 # for i in range(10):
 # robot.right_forward()
