f010082d302a9ef8bb9928c52a08854d8a1780da
Ezra1285 <ezraskoog@yahoo.com>
2023-04-28 18:19:11 -0600

    fix4

--- test_movement.py
+++ test_movement.py
@@ -1,12 +1,21 @@
 import control_robot
+import maestro
 
-robot = control_robot.robot()
+
+bot = maestro.Controller()
+
+bot.setAccel(0,60)
+bot.setSpeed(0, 10)
+bot.setTarget(0, 6000)
+
+bot.setTarget(2, 6800)
+# robot = control_robot.robot()
 # for i in range(10):
-robot.right_forward()
-robot.right()
-robot.right_forward()
-robot.right()
+# robot.right_forward()
+# robot.right()
+# robot.right_forward()
+# robot.right()
     # print("Loop " , i)
     # robot.waistRight()
 
-robot.stop()
+# robot.stop()
