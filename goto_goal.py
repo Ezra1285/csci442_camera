bf1e85a94e87df0ee6a79beaf30b52a7373de879
Ezra1285 <ezraskoog@yahoo.com>
2023-05-02 17:55:37 -0600

    goto_goal12

--- goto_goal.py
+++ goto_goal.py
@@ -53,6 +53,7 @@ def handleColor(color_image):
     contours, hierarchy = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for pic, contour in enumerate(contours):
         area = cv2.contourArea(contour)
+        print("AREA is ", area)
         if(area > 3200):
             robot.stopSpin()
             color_found = "green"
