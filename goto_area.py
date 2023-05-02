945846dc5b25943f073b2456c4ef618bd11eecb3
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2023-05-02 17:46:36 -0600

    goto area

--- goto_area.py
+++ goto_area.py
@@ -131,7 +131,7 @@ def goto_mine(edge, line_color, spin_flag):
         print("MOVING")
         if ydif >50:
             move_forward()
-            if(total <1):
+            if(x + y<1):
                 stop()
                 return "done", spin_flag
         elif ydif <0:
@@ -153,7 +153,7 @@ def goto_mine(edge, line_color, spin_flag):
         else:
             stop()
             print("bad")
-        if(total <1):
+        if(x+y <1):
             stop()
             return "done", spin_flag
         # cv2.circle(edge, cof, 10, (255,0,0), 5)
