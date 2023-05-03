c76b2a0f698fe0b0eb432d2264b0cf8617c72f80
Ezra1285 <ezraskoog@yahoo.com>
2023-05-02 19:48:02 -0600

    put together1

--- find_humans.py
+++ find_humans.py
@@ -6,51 +6,50 @@ import control_robot
 import time
 
 # Configure depth and color streams
-pipeline = rs.pipeline()
-config = rs.config()
+# pipeline = rs.pipeline()
+# config = rs.config()
 
 # Get device product line for setting a supporting resolution
-pipeline_wrapper = rs.pipeline_wrapper(pipeline)
-pipeline_profile = config.resolve(pipeline_wrapper)
-device = pipeline_profile.get_device()
-device_product_line = str(device.get_info(rs.camera_info.product_line))
+# pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+# pipeline_profile = config.resolve(pipeline_wrapper)
+# device = pipeline_profile.get_device()
+# device_product_line = str(device.get_info(rs.camera_info.product_line))
 
-found_rgb = False
-for s in device.sensors:
-    if s.get_info(rs.camera_info.name) == 'RGB Camera':
-        found_rgb = True
-        break
-if not found_rgb:
-    print("The demo requires Depth camera with Color sensor")
-    exit(0)
+# found_rgb = False
+# for s in device.sensors:
+#     if s.get_info(rs.camera_info.name) == 'RGB Camera':
+#         found_rgb = True
+#         break
+# if not found_rgb:
+#     print("The demo requires Depth camera with Color sensor")
+#     exit(0)
 
-config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+# config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 
-if device_product_line == 'L500':
-    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
-else:
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+# if device_product_line == 'L500':
+#     config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+# else:
+#     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Start streaming
-profile = pipeline.start(config)
-time.sleep(2)
+# profile = pipeline.start(config)
 robot = control_robot.robot()
 
 #  Set up depth to find how far our person with ice is.
-frames = pipeline.wait_for_frames()
-depth_frame = frames.get_depth_frame()
-color_frame = frames.get_color_frame()
-color = np.asanyarray(color_frame.get_data())
+# frames = pipeline.wait_for_frames()
+# depth_frame = frames.get_depth_frame()
+# color_frame = frames.get_color_frame()
+# color = np.asanyarray(color_frame.get_data())
 tracker = cv2.TrackerKCF_create()
 # bbox = (287, 23, 86, 320)
 # Getting the depth sensor's depth scale (see rs-align example for explanation)
-depth_sensor = profile.get_device().first_depth_sensor()
-depth_scale = depth_sensor.get_depth_scale()
+# depth_sensor = profile.get_device().first_depth_sensor()
+# depth_scale = depth_sensor.get_depth_scale()
 #  Create an align object
-align_to = rs.stream.color
-align = rs.align(align_to)
+# align_to = rs.stream.color
+# align = rs.align(align_to)
 # bbox = cv2.selectROI(color, False)
 # Initialize tracker with first frame and bounding box
 # isFaceFound = False
