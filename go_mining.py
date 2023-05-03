b94bccb3ed5bf9bd7901f972d584b7c97d11364e
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2023-05-03 10:24:35 -0600

    add goal

--- go_mining.py
+++ go_mining.py
@@ -53,9 +53,11 @@ class ctrl_methods():
         if(self.method_num ==0):
             ret, self.spin_flag = goto_area.goto_mine(color, "blue", self.spin_flag)
         elif(self.method_num ==1):
-            ret, color_found = find_humans.findHumans(frames)
+            ret, self.color_found = find_humans.findHumans(frames)
         elif(self.method_num ==2):
             ret, self.spin_flag = goto_area.goto_mine(color, "orange", self.spin_flag)
+        elif(self.method_num ==3):
+            ret, self.spin_flag = goto_area.goto_mine(self.color_found, self.color_found, self.spin_flag)
         else:
             raise Exception("Done!")
         if(ret == "done"):
