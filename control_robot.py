5c6a7dd73331f29b6d000f52d4945059795a31b4
Ezra1285 <ezraskoog@yahoo.com>
2023-05-02 20:12:37 -0600

    put together8

--- control_robot.py
+++ control_robot.py
@@ -104,7 +104,7 @@ class robot:
         self.robot_controll.setTarget(4, self.headTilt)
 
     def centerHead(self):
-        self.robot_controll.setTarget(4, 2200)
+        self.robot_controll.setTarget(4, 2750)
 
     def headDown(self):
         self.headTilt -= 200
