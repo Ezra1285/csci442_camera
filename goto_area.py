6d6f09512921c127ee874a20fc4037079c040d1c
Baiden-M <99929085+Baiden-M@users.noreply.github.com>
2023-05-02 19:05:22 -0600

    goto area

--- goto_area.py
+++ goto_area.py
@@ -124,7 +124,7 @@ def goto_mine(edge, line_color, spin_flag):
             total = 1
         yavg = int(x/total)
         xavg = int(y/total)
-        cofy = int((350))
+        cofy = int((375))
         cofx = int(150)
         cof = (cofx, cofy)
         # cv2.circle(edge, cof, 10, (255,0,0), 5)
